2fa3b19df32ccc2ab67163fa957d212bf6df3b6b
yetanotherautist <jakubpart@gmail.com>
2020-06-07 02:08:36 +0200

    TODO added, basic schema created

--- app/Main.hs
+++ app/Main.hs
@@ -3,4 +3,43 @@ module Main where
 import Lib
 
 main :: IO ()
-main = someFunc
+main = do
+    putStrLn "Haskell genalg"
+    putStrLn "Podaj wyraz"
+    sWord <- getLine
+    putStrLn "Size of population"
+    sSize <- getLine
+    putStrLn "Generations"
+    sGen <- getLine
+    putStrLn "Mutation chance"
+    sMutChan <- getLine
+    putStrLn "Crossing chance"
+    sCrossChan <- getLine
+    let size = read sSize :: Int
+        gen = read sGen :: Int
+--        mutchan = read sMutChan :: Float
+--        crosschan = read sCrossChan :: Float
+--        chromosomes = length sWord
+--    genPop size chromosomes
+    putStrLn sWord
+--    putStrLn gen
+--    putStrLn mutchan
+--    putStrLn crosschan
+--    putStrLn chromosomes
+    someFunc
+
+--TODO: sama funkcja algorytmu genetycznego (sklejenie powyższych funkcji tak, by było wykonywane)
+--OGÓLNY TEMPLATE
+--generuj
+--oceń
+--while przystosowanie != 1.0 or iter_index >=1000 do:
+--  print (iter_index, przystosowanie)
+--  selekcja
+--  krzyżowanie
+--  mutacja
+--return
+--skonwertuj z ASCII na tekst
+
+
+--genalg ::
+--genalg =
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,59 @@
 module Lib
-    ( someFunc
+    ( Ind (..)
+    , Pop (..)
+    , genInd
+    , genPop
+    , someFunc
     ) where
 
+
+--import System.Random
+
+
+data Ind =
+    Ind { chromosome :: [Int], fitness :: Int }
+    deriving(Show, Read, Eq)
+
+data Pop =
+    Pop { size :: Int, inds :: [Ind] }
+    deriving(Show, Read, Eq)
+
+--TODO: text to ASCII
+--pobiera zdanie podane jako input, zwraca listę z numerami ascii znaków
+
+--TODO: ASCII to text
+--jak wyżej, tylko odwrotnie
+
+--TODO: generowanie populacji
+-- input to Int = n osobników, zwraca obiekt Pop z n * Ind
+
+genPop :: Int -> Int -> Pop
+genPop s r = Pop s fs
+    where
+        fs = [genInd s | x <- [1..s]]
+
+genInd :: Int -> Ind
+genInd s = Ind fs 0
+    where
+        fs = [ 1 | y <- [1..s]]
+        
+--TODO: ocena przystosowania
+--fit_eval :: Pop -> Pop
+
+--TODO: selekcja
+--selekcja rankingowa,
+--selection :: Pop -> Pop
+
+--TODO: krzyżowanie
+--crossing :: Float -> Pop -> Pop
+
+--TODO: mutacja
+--mutation :: Float -> Pop -> Pop
+
+
+
+
+
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = putStrLn "Over"
