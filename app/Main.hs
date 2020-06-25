6f9dded821cf0b9b19d1f8be52a451ca35a5e280
yetanotherautist <jakubpart@gmail.com>
2020-06-25 13:17:08 +0200

    Dodane funkcje Dawida i podstawowe losowanie

--- app/Main.hs
+++ app/Main.hs
@@ -1,28 +1,60 @@
 module Main where
 
 import Lib
+import System.Random
 
 main :: IO ()
 main = do
-    putStrLn "Haskell genalg"
-    putStrLn "Add word to decipher"
+
+    --gen <- newStdGen
+    
+    --take 1 $ randomRs (0, 10) gen
+    -- powinno wziąć 1 losowa liczbe z przedzialu 0-10
+
+    --print $ randomList 5 0 10 gen
+    -- powinno wygenerowac 5-elementowa, losowa liste z przedzialu 0-10
+    
+    putStrLn "Algorytm genetyczny"
+    
+    putStrLn "Podaj słowo docelowe: "
     sWord <- getLine
-    putStrLn "Size of population"
+    putStrLn "Podaj rozmiar populacji: "
     sSize <- getLine
-    putStrLn "Generations"
+    putStrLn "Podaj maksymalna liczbe generacji: "
     sGen <- getLine
-    putStrLn "Mutation chance"
+    putStrLn "Podaj szanse na mutacje: "
     sMutChan <- getLine
-    putStrLn "Crossing chance"
+    putStrLn "Podaj szanse na skrzyzowanie: "
     sCrossChan <- getLine
+    
     let size = read sSize :: Int
-        gen = read sGen :: Int
+        geny = read sGen :: Int
         mutchan = read sMutChan :: Float
         crosschan = read sCrossChan :: Float
         chromosomes = length sWord
-        pop = genPop size chromosomes
-    putStrLn $ show pop
+        --pop = genPop size chromosomes
+        
+    --putStrLn $ show pop
     someFunc
+
+-- tutaj musimy wygenrować populacje, wg podanych wyżej parametrow (liczba osobnikow, ilosc genow)
+
+-- potem, gdy juz ja bedziemy mieli - cos takiego: [[Int]], to jesli chcemy wywolywac funkcje na losowych osobnikach, to robimy tak: [[x]] !! y - gdzie y to jakas nasza wylosowana liczba oznaczajaca osobnika
+
+-- bedziemy musieli takie liczby losowac do mutacji - zeby wylosowac osobnika, gen, ktory chcemy zmienic oraz na co ma on zostac zmieniony
+
+-- oraz do krzyzowania, wtedy musimy wylosowac dwoch osobnikow oraz miejsce, w ktorym maja sie przeciac
+
+-- nastepnie wywolujemy selekcje, ktora nam stwarza nowa populacje
+
+-- i oceniamy przystosowanie osobnikow, jesli roznica miedzy jakims osobnikiem a naszym celem - podanym slowem wynosi 0 to zamykamy program, jesli nie to od nowa
+
+-- to wszystko generealnie bedziemy musieli robic tu w mainie, korzystajac z pojedynczych funkcji z lib
+
+
+
+
+
 
 --TODO: sama funkcja algorytmu genetycznego (sklejenie powyższych funkcji tak, by było wykonywane)
 --OGÓLNY TEMPLATE
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,65 +1,91 @@
+{-
+Module      : Lib
+Description : Genetic algorythm
+Copyright   : Czech D., 2020
+              Partyka J., 2020
+              Sołtysiak W., 2020
+License     : GPL-2
+Maintainer  : jakubpart@gmail.com
+Stability   : unstable %nie wiem, może będzie stable
+Portability : POSIX
+-}
+
 module Lib
-    ( Ind (..)
-    , Pop (..)
-    , genInd
-    , genPop
+    ( t2a
+    , a2t
+    , fitness
+    , mutacja
+    , krzyzowanie
+    , wyciagnij
     , someFunc
+    , genList
+   -- , randomList
     ) where
 
---import System.Random (randomR)
+
+import Data.Char
+import System.Random
 
 
-data Ind =
-    Ind { chromosome :: [Int], fitness :: Int }
-    deriving(Show, Read, Eq)
-
-data Pop =
-    Pop { size :: Int, inds :: [Ind] }
-    deriving(Show, Read, Eq)
-
---TODO: text to ASCII
---pobiera zdanie podane jako input, zwraca listę z numerami ascii znaków
-
---TODO: ASCII to text
---jak wyżej, tylko odwrotnie
-
---TODO: generowanie populacji
--- input to Int = n osobników, zwraca obiekt Pop z n * Ind
-
-genPop :: Int -> Int -> Pop
-genPop s r = Pop s fs
-    where
-        fs = [genInd r | x <- [1..s]]
-
-genInd :: Int -> Ind
-genInd s = Ind fs 0
-    where
-        fs = [ 1 | y <- [1..s]]
-        
---TODO: ocena przystosowania
---fit_eval :: Pop -> Pop
-
---TODO: selekcja
---selekcja rankingowa,
---selection :: Pop -> Pop
-
---for i in range(populacja):
---    a = losowy pierwszy woj
---    b = losowy drugi woj
---    if a_fit>b_fit:
---        append_nowa_pop(a)
---    else:
---        append_nowa_pop(b)
-
---TODO: krzyżowanie
---crossing :: Float -> Pop -> Pop
-
---TODO: mutacja
---mutation :: Float -> Pop -> Pop
+--genList :: Int -> [Int]
+genList n = sequence $ replicate n $ randomRIO (1,32::Int)
+--genpop :: Int -> Int -> [[Int]]
+--genpop s r = [a | x <- [1..s], a <- genList r]
+--geneList n = do (x <- genList n)
+--    x
 
 
+-- | Zamiana stringow na liste intow
+-- = self explanatory, dziala na male i duze litery
+t2a :: [Char] -> [Int]
+t2a [] = []
+t2a (x:xs) = [ord x - ord 'a'] ++ t2a xs
 
 
---x = getStdRandom (randomR (1,6))
+-- | Zamiana listy intow na stringi
+-- = self explanatory, dziala na male i duze litery
+a2t :: [Int] -> [Char]
+a2t [] = []
+a2t (x:xs) = [chr (ord 'a' + x)] ++ a2t xs
+
+
+-- | Przystosowanie osobnika
+-- = Funkcja sprawdza jak podobny jest podany osobnik do wyznaczonego wzorca
+-- == Funkcja pobiera dwie listy: pierwsza wzorcowa, a druga do porownania z nia, zwraca liczbe oznaczajaca sume "odleglosci" wszystkich miejsc w liscie porownywanej od wzorca
+fitness :: [Int] -> [Int] -> Int
+fitness [] [] = 0
+fitness (x:xs) (y:ys) = (abs(x-y)) + fitness xs ys
+
+
+-- | Mutacja osobnika
+-- = Funkcja zmienia jeden z genow osobnika, na nowy
+-- == Funkcja pobiera osobnika - liste, liczbe oznaczajaca pozycje w liscie (liczac od 0), ktora ma ulec zmianie oraz nowy element, ktory ma sie znalezc w podanym miejscu, po czym zwraca zmodyfikowana liste
+mutacja :: [Int] -> Int -> Int -> [Int]
+mutacja xs y z = take y xs ++ [z] ++ (reverse(take ((length xs) - (y + 1)) (reverse xs)))
+
+
+-- | Mutacja osobnikow
+-- = Funkcja krzyzuje dwoch osobnikow w wyznaczonym miejscu
+-- == Funkcja pobiera dwoch osobnikow - listy, liczbe oznaczajaca miejsce przeciecia sie tych dwoch list (liczac od 0), a nastepnie zwraca pare uporzadkowana z tymi dwoma osobnikami po tym, jak wyznaczone czesci tych list zamienily sie miejscami
+krzyzowanie :: [Int] -> [Int] -> Int -> ([Int],[Int])
+krzyzowanie xs ys z = (take z xs ++ (reverse(take ((length ys) - z) (reverse ys))), take z ys ++ (reverse(take ((length xs) - z) (reverse xs))))
+
+
+-- | Wyciaganie pojedynczego osobnika z pary uporzadkowanej
+-- = Funkcja "wyciaga" pierwszego lub drugiego osobnika z pary uporzadkwanej
+-- == Funkcja pobiera pare uporzadkowana oraz liczbe oznaczajaca, ktory element tej pary zostanie "wyciagniety". 0 oznacza pierwszy element, a jakakolwiek inna liczba drugi element
+wyciagnij :: ([Int],[Int]) -> Int -> [Int]
+wyciagnij (xs,ys) z = if z == 0 then xs else ys
+
+
+-- | Pojedynek na przystosowanie dwoch osobnikow
+-- = Funkcja porownuje dwoch osobnikow pod wzgledem ich przystosowania (zblizenia do podanego wzorca)
+-- == Funkcja pobiera docelowy wzorzec, a nastepnie dwoch osobnikow, nastepnie porownuje przystosowania tych dwoch osobnikow (ich podobienstwo do wzorca), by zwrocic bardziej przystosowanego - tego z mniejsca roznica od wzorca
+turniej :: [Int] -> [Int] -> [Int] -> [Int]
+turniej [] [] [] = []
+turniej x y z = if (fitness x y) < (fitness x z) then y else z
+
+-- TODO genalg, typ będzie jakiś taki :: [[Int]] -> [Int] -> ... -> [[Int]], gdzie po kolei mamy: stara populacja, wzorzecz, cos tam cookolwiek bedzie potrzebne do wywoylania innych funkcji, nowa populacja
+
 someFunc :: IO ()
 someFunc = putStrLn "Over"
